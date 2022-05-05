159622ddd8c5a13c97f1cbdb029da58392118d89
Manish Adhikari <adhikari.manish555@g,ail.com>
2022-05-05 18:42:55 +0545

    break is added

--- Prime Number.py
+++ Prime Number.py
@@ -1,13 +1,14 @@
 x = int(input('check if prime?'))
 a = 0
 for i in range(1, x + 1):
+    if a>2:
+        print('this number is Composite')
+        break
     if x % i == 0:
         a = a + 1
 
 if (a == 2):
     print("The number is prime ")
-else:
-    print("The number is composite")
 
 #my first prject
 
